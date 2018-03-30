8bad4863c326f787dbbd03bf7644c921d4e1f534
Nikola Babanic <babanicnikola@gmail.com>
2018-03-30 20:40:55 +0200

    Update coder.pas

--- coder.pas
+++ coder.pas
@@ -1,14 +1,12 @@
-PROGRAM coder;
+PROGRAM textToAscii;
 USES
         Sysutils,
         crt;
 
 VAR
         text : STRING;
-        i, j, x, y : LONGINT;
-        ascii : ARRAY [1..1000] OF LONGINT;
+        i : LONGINT;
 BEGIN
-        j := 1;
         TextBackground(White);
         TextColor(Black);
         ClrScr();
@@ -19,12 +17,8 @@ BEGIN
         writeln('--------------------------------------------------------------');
         write('ASCII Code: ');
         FOR i := 1 TO length(text) DO
-                Begin
                         Write (ord(text[i]), ' ');
-                        ascii[i] := ord(text[i]);
-                End;
         writeln();
-
         writeln('--------------------------------------------------------------');
         writeln('Press any key to exit!');
         readkey();
